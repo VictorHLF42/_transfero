df2b02e4fd716febd2df40f379a4eede2e73297d
Seu Nome <seu_email@example.com>
2025-03-24 16:45:41 -0300

    24/03

--- sistema/models.py
+++ sistema/models.py
@@ -1,3 +1,13 @@
+from django.utils import timezone
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):  # Corrigido: "models.Model"
+    nome = models.CharField(max_length=50)
+    sobrenome = models.CharField(max_length=50)
+    cpf = models.CharField(max_length=11)
+    telefone = models.CharField(max_length=20)
+    email = models.EmailField(max_length=254)
+    endereco = models.CharField(max_length=100)
+    data_cadastro = models.DateTimeField(default=timezone.now)
+    ativo = models.BooleanField(default=True)  # Corrigido: removido "models" extra
+    # foto = models.ImageField(upload_to='usuarios/')  # Campo opcional (descomente se quiser usar)
--- sistema/urls.py
+++ sistema/urls.py
@@ -1,7 +1,7 @@
 
 from django.urls import path
 from sistema import views
-
+#informa qual será a rota que ele irá chamaar determinada view(função).
 urlpatterns = [
     path('sistema/', views.index),
 ]
