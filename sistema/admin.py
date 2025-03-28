11323b92395af46ad2ba876de136defb58085c5b
Seu Nome <seu_email@example.com>
2025-03-28 16:28:08 -0300

    28/03

--- sistema/admin.py
+++ sistema/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
+from .models import Usuario, Filme, Genero
 
-from sistema import models
-
-# aqui fica o registro do Usuário
-@admin.register(models.Usuario)
 class UsuarioAdmin(admin.ModelAdmin):
-    list_display = ('id', 'nome', 'sobrenome', 'email', 'ativo',)
+    list_display = ('nome', 'sobrenome', 'email', 'data_cadastro', 'ativo')
+
+class FilmeAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'ano', 'estudio', 'data_cadastro')
+
+class GeneroAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'data_cadastro')
+
+admin.site.register(Usuario, UsuarioAdmin)
+admin.site.register(Filme, FilmeAdmin)
+admin.site.register(Genero, GeneroAdmin)
--- sistema/models.py
+++ sistema/models.py
@@ -1,7 +1,7 @@
 from django.utils import timezone
 from django.db import models
 
-class Usuario(models.Model):  # Corrigido: "models.Model"
+class Usuario(models.Model):
     nome = models.CharField(max_length=50)
     sobrenome = models.CharField(max_length=50)
     cpf = models.CharField(max_length=11)
@@ -9,5 +9,24 @@ class Usuario(models.Model):  # Corrigido: "models.Model"
     email = models.EmailField(max_length=254)
     endereco = models.CharField(max_length=100)
     data_cadastro = models.DateTimeField(default=timezone.now)
-    ativo = models.BooleanField(default=True)  # Corrigido: removido "models" extra
-    # foto = models.ImageField(upload_to='usuarios/')  # Campo opcional (descomente se quiser usar)
+    ativo = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nome
+
+class Filme(models.Model):
+    nome = models.CharField(max_length=50)
+    ano = models.DateField(default=timezone.now)
+    estudio = models.CharField(max_length=50)
+    sinopse = models.TextField()
+    data_cadastro = models.DateField(default=timezone.now)
+
+    def __str__(self):
+        return self.nome
+
+class Genero(models.Model):
+    nome = models.CharField(max_length=50)  # Corrigido para usar '='
+    data_cadastro = models.DateField(default=timezone.now)  # Corrigido para definir um valor padrão
+
+    def __str__(self):
+        return self.nome
